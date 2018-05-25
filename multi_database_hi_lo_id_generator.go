eb319f2ec388d00f9dfc17b4a6263b2612730db9
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-05-24 17:18:23 -0700

    rename MultiTypeHiLoKeyGenerator => MultiTypeHiLoIDGenerator; move to its own file

--- multi_database_hi_lo_id_generator.go
+++ multi_database_hi_lo_id_generator.go
@@ -3,14 +3,14 @@ package ravendb
 // MultiDatabaseHiLoIDGenerator manages per-database HiLoKeyGenerotr
 type MultiDatabaseHiLoIDGenerator struct {
 	store      *DocumentStore
-	generators map[string]*MultiTypeHiLoKeyGenerator
+	generators map[string]*MultiTypeHiLoIDGenerator
 }
 
 // NewMultiDatabaseHiLoIDGenerator creates new MultiDatabaseHiLoKeyGenerator
 func NewMultiDatabaseHiLoIDGenerator(store *DocumentStore) *MultiDatabaseHiLoIDGenerator {
 	return &MultiDatabaseHiLoIDGenerator{
 		store:      store,
-		generators: map[string]*MultiTypeHiLoKeyGenerator{},
+		generators: map[string]*MultiTypeHiLoIDGenerator{},
 	}
 }
 
@@ -22,10 +22,10 @@ func (g *MultiDatabaseHiLoIDGenerator) GenerateDocumentID(dbName string, entity 
 	panicIf(dbName == "", "expected non-empty dbName")
 	generator, ok := g.generators[dbName]
 	if !ok {
-		generator = NewMultiTypeHiLoKeyGenerator(g.store, dbName)
+		generator = NewMultiTypeHiLoIDGenerator(g.store, dbName)
 		g.generators[dbName] = generator
 	}
-	return generator.GenerateDocumentKey(entity)
+	return generator.GenerateDocumentID(entity)
 }
 
 // ReturnUnusedRange returns unused range for all generators
---
+++ multi_type_hi_lo_id_generator.go
@@ -0,0 +1,43 @@
+package ravendb
+
+import "sync"
+
+// MultiTypeHiLoIDGenerator manages per-type HiLoKeyGenerator
+type MultiTypeHiLoIDGenerator struct {
+	store  *DocumentStore
+	dbName string
+	// maps type name to its generator
+	_idGeneratorsByTag map[string]*HiLoKeyGenerator
+	lock               sync.Mutex // protects _idGeneratorsByTag
+	// TODO: conventions
+}
+
+// NewMultiTypeHiLoIDGenerator creates MultiTypeHiLoKeyGenerator
+func NewMultiTypeHiLoIDGenerator(store *DocumentStore, dbName string) *MultiTypeHiLoIDGenerator {
+	return &MultiTypeHiLoIDGenerator{
+		store:              store,
+		dbName:             dbName,
+		_idGeneratorsByTag: map[string]*HiLoKeyGenerator{},
+	}
+}
+
+// GenerateDocumentID generates a unique key for entity using its type to
+// partition keys
+func (g *MultiTypeHiLoIDGenerator) GenerateDocumentID(entity interface{}) string {
+	tag := defaultTransformTypeTagName(getShortTypeName(entity))
+	g.lock.Lock()
+	generator, ok := g._idGeneratorsByTag[tag]
+	if !ok {
+		generator = NewHiLoKeyGenerator(tag, g.store, g.dbName)
+		g._idGeneratorsByTag[tag] = generator
+	}
+	g.lock.Unlock()
+	return generator.GenerateDocumentKey()
+}
+
+// ReturnUnusedRange returns unused range for all generators
+func (g *MultiTypeHiLoIDGenerator) ReturnUnusedRange() {
+	for _, generator := range g._idGeneratorsByTag {
+		generator.ReturnUnusedRange()
+	}
+}
