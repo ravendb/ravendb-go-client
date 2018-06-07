a7054a59727bf57e18fa4a7515f1b111402d8698
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-06-07 00:53:25 -0700

    progress on Operation

--- operation.go
+++ operation.go
@@ -1,5 +1,7 @@
 package ravendb
 
+import "time"
+
 type Operation struct {
 	_requestExecutor *RequestExecutor
 	//TBD private readonly Func<IDatabaseChanges> _changes;
@@ -17,5 +19,42 @@ func NewOperation(requestExecutor *RequestExecutor, changes *IDatabaseChanges, c
 		//TBD _changes = changes;
 		_conventions: conventions,
 		_id:          id,
+	}
+}
+
+func (o *Operation) fetchOperationsStatus() ObjectNode {
+	command := o.getOperationStateCommand(o._conventions, o._id)
+	o._requestExecutor.executeCommand(command)
+	resi := command.getResult()
+	res := resi.(ObjectNode)
+	return res
+}
+
+func (o *Operation) getOperationStateCommand(conventions *DocumentConventions, id int) *RavenCommand {
+	return NewGetOperationStateCommand(o._conventions, o._id)
+}
+
+func (o *Operation) waitForCompletion() error {
+	for {
+		status := o.fetchOperationsStatus()
+
+		operationStatus := jsonGetAsText(status, "Status")
+		switch operationStatus {
+		case "Completed":
+			return nil
+		case "Cancelled":
+			return NewOperationCancelledException("")
+		case "Faulted":
+			panicIf(true, "NYI")
+			/*
+				result := status["Result"]
+
+				OperationExceptionResult exceptionResult = JsonExtensions.getDefaultMapper().convertValue(result, OperationExceptionResult.class);
+
+				throw ExceptionDispatcher.get(exceptionResult.getMessage(), exceptionResult.getError(), exceptionResult.getType(), exceptionResult.getStatusCode());
+			*/
+		}
+
+		time.Sleep(500 * time.Millisecond)
 	}
 }
---
+++ operation_exception_result.go
@@ -0,0 +1,42 @@
+package ravendb
+
+type OperationExceptionResult struct {
+	Type       string `json:"Type"`
+	Message    string `json:"Message"`
+	Error      string `json:"Error"`
+	StatusCode int    `json:"StatusCode"`
+}
+
+/*
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+	}
+*/
