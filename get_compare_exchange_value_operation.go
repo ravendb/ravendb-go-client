6723b18033ab6b4573153739464b73b4c58d30b6
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-06-27 14:02:54 -0700

    GetCompareExchangeValuesOperation

--- get_compare_exchange_value_operation.go
+++ get_compare_exchange_value_operation.go
@@ -3,99 +3,64 @@ package ravendb
 import (
 	"net/http"
 	"reflect"
-	"strconv"
 )
 
 var (
-	_ IOperation = &GetCompareExchangeValuesOperation{}
+	_ IOperation = &GetCompareExchangeValueOperation{}
 )
 
-type GetCompareExchangeValuesOperation struct {
-	Command *GetCompareExchangeValuesCommand
+type GetCompareExchangeValueOperation struct {
+	Command *GetCompareExchangeValueCommand
 
+	_key   string
 	_clazz reflect.Type
-	_keys  []string
-
-	_startWith string
-	_start     int // -1 for unset
-	_pageSize  int
 }
 
-func NewGetCompareExchangeValuesOperationWithKeys(clazz reflect.Type, keys []string) *GetCompareExchangeValuesOperation {
-	// TODO: validate
-	return &GetCompareExchangeValuesOperation{
-		_keys:  keys,
+func NewGetCompareExchangeValueOperation(clazz reflect.Type, key string) *GetCompareExchangeValueOperation {
+	return &GetCompareExchangeValueOperation{
 		_clazz: clazz,
-
-		_start:     -1,
-		_pageSize:  0,
-		_startWith: "",
+		_key:   key,
 	}
 }
 
-func NewGetCompareExchangeValuesOperation(clazz reflect.Type, startWith string, start int, pageSize int) *GetCompareExchangeValuesOperation {
-	return &GetCompareExchangeValuesOperation{
-		_clazz: clazz,
-
-		_start:     start,
-		_pageSize:  pageSize,
-		_startWith: "",
-	}
-
-}
-
-var _ RavenCommand = &GetCompareExchangeValuesCommand{}
-
-func (o *GetCompareExchangeValuesOperation) getCommand(store *IDocumentStore, conventions *DocumentConventions, cache *HttpCache) RavenCommand {
-	o.Command = NewGetCompareExchangeValuesCommand(o, conventions)
+func (o *GetCompareExchangeValueOperation) getCommand(store *IDocumentStore, conventions *DocumentConventions, cache *HttpCache) RavenCommand {
+	o.Command = NewGetCompareExchangeValueCommand(o._clazz, o._key, conventions)
 	return o.Command
 }
 
-type GetCompareExchangeValuesCommand struct {
+var _ RavenCommand = &GetCompareExchangeValueCommand{}
+
+type GetCompareExchangeValueCommand struct {
 	*RavenCommandBase
 
-	_operation   *GetCompareExchangeValuesOperation
+	_key         string
+	_clazz       reflect.Type
 	_conventions *DocumentConventions
-	Result       map[string]*CompareExchangeValue
+
+	Result *CompareExchangeValue
 }
 
-func NewGetCompareExchangeValuesCommand(operation *GetCompareExchangeValuesOperation, conventions *DocumentConventions) *GetCompareExchangeValuesCommand {
-	cmd := &GetCompareExchangeValuesCommand{
+func NewGetCompareExchangeValueCommand(clazz reflect.Type, key string, conventions *DocumentConventions) *GetCompareExchangeValueCommand {
+	// TODO: validation
+	cmd := &GetCompareExchangeValueCommand{
 		RavenCommandBase: NewRavenCommandBase(),
 
-		_operation:   operation,
+		_clazz:       clazz,
+		_key:         key,
 		_conventions: conventions,
 	}
 	cmd.IsReadRequest = true
 	return cmd
 }
 
-func (c *GetCompareExchangeValuesCommand) createRequest(node *ServerNode) (*http.Request, error) {
-	url := node.getUrl() + "/databases/" + node.getDatabase() + "/cmpxchg?"
+func (c *GetCompareExchangeValueCommand) createRequest(node *ServerNode) (*http.Request, error) {
+	url := node.getUrl() + "/databases/" + node.getDatabase() + "/cmpxchg?key=" + urlEncode(c._key)
+	return NewHttpGet(url)
 
-	if c._operation._keys != nil {
-		for _, key := range c._operation._keys {
-			url += "&key=" + UrlUtils_escapeDataString(key)
-		}
-	} else {
-		if !StringUtils_isEmpty(c._operation._startWith) {
-			url += "&startsWith=" + UrlUtils_escapeDataString(c._operation._startWith)
-		}
-
-		if c._operation._start >= 0 {
-			url += "&start=" + strconv.Itoa(c._operation._start)
-		}
-
-		if c._operation._pageSize > 0 {
-			url += "&pageSize=" + strconv.Itoa(c._operation._pageSize)
-		}
-	}
-
-	return NewHttpDelete(url, nil)
 }
 
-func (c *GetCompareExchangeValuesCommand) setResponse(response []byte, fromCache bool) error {
-	res, err := CompareExchangeValueResultParser_getValues(c._operation._clazz, response, c._conventions)
+func (c *GetCompareExchangeValueCommand) setResponse(response []byte, fromCache bool) error {
+	res, err := CompareExchangeValueResultParser_getValue(c._clazz, response, c._conventions)
 	if err != nil {
 		return err
 	}
