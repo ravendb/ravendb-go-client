98c20340a6b85f31bbc5149c0f92417816ddfaa8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-06-28 20:41:28 -0700

    IndexOperationsTest.canListErrors

--- index_operations_test.go
+++ index_operations_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func NewUsersInvalidIndex() *AbstractIndexCreationTask {
+	res := NewAbstractIndexCreationTask("UsersInvalidIndex")
+	res.smap = "from u in docs.Users select new { a = 5 / u.Age }"
+	return res
+}
+
 func testIndexCanDeleteIndex(t *testing.T) {
 }
 
@@ -54,7 +60,41 @@ func testIndexCanSetIndexLockMode(t *testing.T) {
 func testIndexCanSetIndexPriority(t *testing.T) {
 }
 func testIndexCanListErrors(t *testing.T) {
+	var err error
+	store := getDocumentStoreMust(t)
+	index := NewUsersInvalidIndex()
+	indexDef := index.createIndexDefinition()
+	op := NewPutIndexesOperation(indexDef)
+	err = store.maintenance().send(op)
+	assert.NoError(t, err)
+
+	{
+		session := openSessionMust(t, store)
+		user := NewUser()
+		//user.setName(null)
+		user.setAge(0)
+		err = session.StoreEntity(user)
+		assert.NoError(t, err)
+		err = session.SaveChanges()
+		assert.NoError(t, err)
+	}
+
+	err = gRavenTestDriver.waitForIndexing(store, store.getDatabase(), 0)
+	assert.NoError(t, err)
+
+	op2 := NewGetIndexErrorsOperation(nil)
+	err = store.maintenance().send(op2)
+	assert.NoError(t, err)
+	indexErrors := op2.Command.Result
+	assert.Equal(t, len(indexErrors), 1)
+
+	op3 := NewGetIndexErrorsOperation([]string{indexDef.getName()})
+	err = store.maintenance().send(op3)
+	assert.NoError(t, err)
+	perIndexErrors := op3.Command.Result
+	assert.Equal(t, len(perIndexErrors), 1)
 }
+
 func testIndexCanGetIndexStatistics(t *testing.T) {
 }
 
