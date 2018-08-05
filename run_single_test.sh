9c224d5a5c14952ce92d8aa91b0fee2dba111036
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-08-04 18:08:40 -0700

    run tests with -race; check taht capturer and pcap_convert are built

--- run_single_test.sh
+++ run_single_test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-set -o xtrace
 
 # Helper for running a test I'm currently working on
 # Faster than running all tests
+
+if [ ! -f ./capturer ]; then
+    echo "./capturer not found!"
+    echo "Run ./build_capturer.sh to create it"
+    exit 1
+fi
+
+if [ ! -f ./pcap_convert ]; then
+    echo "./pcap_convert not found!"
+    echo "Run ./build_capturer.sh to create it"
+    exit 1
+fi
+
+set -o xtrace
 
 # uncomment for more verbose logging
 export VERBOSE_LOG=true
