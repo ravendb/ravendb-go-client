8639db4334ce18bc2a9743fd94d5865b22d337e4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-08-09 17:08:17 -0700

    bump server version used for test on mac from 4.0.3 to 4.0.6

--- download_mac_and_unpack_locally.sh
+++ download_mac_and_unpack_locally.sh
@@ -3,7 +3,7 @@ set -u -e -o pipefail
 
 rm -rf RavenDB.tar.bz2
 
-wget -O RavenDB.tar.bz2 https://daily-builds.s3.amazonaws.com/RavenDB-4.0.3-osx-x64.tar.bz2
+wget -O RavenDB.tar.bz2 https://daily-builds.s3.amazonaws.com/RavenDB-4.0.6-osx-x64.tar.bz2
 
 # TODO: daily seems broken
 # wget -O RavenDB.tar.bz2 https://hibernatingrhinos.com/downloads/RavenDB%20for%20OSX/latest?buildType=nightly
