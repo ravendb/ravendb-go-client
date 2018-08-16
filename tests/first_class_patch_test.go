bb117ac8e3b8b39f3698719bd16da3a366db47ea
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-08-16 16:24:50 -0700

    FirstClassPatchTest.canPatchComplex

--- tests/first_class_patch_test.go
+++ tests/first_class_patch_test.go
@@ -110,6 +110,7 @@ func firstClassPatch_canPatch(t *testing.T) {
 func firstClassPatch_canPatchAndModify(t *testing.T) {
 	user := &User2{}
 	user.Numbers = []int{66}
+
 	var err error
 	store := getDocumentStoreMust(t)
 	defer store.Close()
@@ -136,11 +137,122 @@ func firstClassPatch_canPatchAndModify(t *testing.T) {
 		assert.NoError(t, err)
 		err = session.SaveChanges()
 		_ = err.(*ravendb.IllegalStateException)
+
 		session.Close()
 	}
 }
 
-func firstClassPatch_canPatchComplex(t *testing.T)       {}
+func firstClassPatch_canPatchComplex(t *testing.T) {
+	stuff := []*Stuff{nil, nil, nil}
+	stuff[0] = &Stuff{
+		Key: 6,
+	}
+
+	user := &User2{
+		Stuff: stuff,
+	}
+
+	var err error
+	store := getDocumentStoreMust(t)
+	defer store.Close()
+
+	{
+		session := openSessionMust(t, store)
+
+		err = session.Store(user)
+		assert.NoError(t, err)
+		err = session.SaveChanges()
+		assert.NoError(t, err)
+
+		session.Close()
+	}
+
+	{
+		session := openSessionMust(t, store)
+
+		phone := "9255864406"
+		newStuff := &Stuff{
+			Key:    4,
+			Phone:  &phone,
+			Friend: &Friend{},
+		}
+		err = session.Advanced().PatchByID(_docId, "stuff[1]", newStuff)
+		assert.NoError(t, err)
+		err = session.SaveChanges()
+		assert.NoError(t, err)
+
+		session.Close()
+	}
+
+	{
+		session := openSessionMust(t, store)
+
+		loadedI, err := session.Load(ravendb.GetTypeOf(&User2{}), _docId)
+		assert.NoError(t, err)
+		loaded := loadedI.(*User2)
+		assert.Equal(t, *loaded.Stuff[1].Phone, "9255864406")
+
+		assert.Equal(t, loaded.Stuff[1].Key, 4)
+		assert.NotNil(t, loaded.Stuff[1].Friend)
+
+		pet1 := &Pet{
+			Kind: "Dog",
+			Name: "Hanan",
+		}
+
+		friendsPet := &Pet{
+			Name: "Miriam",
+			Kind: "Cat",
+		}
+
+		friend := &Friend{
+			Name: "Gonras",
+			Age:  28,
+			Pet:  friendsPet,
+		}
+
+		phone := "9255864406"
+		secondStuff := &Stuff{
+			Key:    4,
+			Phone:  &phone,
+			Pet:    pet1,
+			Friend: friend,
+		}
+
+		m := map[string]string{
+			"Ohio":       "Columbus",
+			"Utah":       "Salt Lake City",
+			"Texas":      "Austin",
+			"California": "Sacramento",
+		}
+
+		secondStuff.Dic = m
+
+		err = session.Advanced().PatchEntity(loaded, "stuff[2]", secondStuff)
+		assert.NoError(t, err)
+		err = session.SaveChanges()
+		assert.NoError(t, err)
+
+		session.Close()
+	}
+
+	{
+		session := openSessionMust(t, store)
+
+		loadedI, err := session.Load(ravendb.GetTypeOf(&User2{}), _docId)
+		assert.NoError(t, err)
+		loaded := loadedI.(*User2)
+
+		assert.Equal(t, loaded.Stuff[2].Pet.Name, "Hanan")
+		assert.Equal(t, loaded.Stuff[2].Friend.Name, "Gonras")
+		assert.Equal(t, loaded.Stuff[2].Friend.Pet.Name, "Miriam")
+		assert.Equal(t, len(loaded.Stuff[2].Dic), 4)
+		assert.Equal(t, loaded.Stuff[2].Dic["Utah"], "Salt Lake City")
+
+		session.Close()
+	}
+}
+
 func firstClassPatch_canAddToArray(t *testing.T)         {}
 func firstClassPatch_canRemoveFromArray(t *testing.T)    {}
 func firstClassPatch_canIncrement(t *testing.T)          {}
