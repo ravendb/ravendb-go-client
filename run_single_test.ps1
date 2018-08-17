f5da0f266b272f7de023b131d3d074f6f28fd6cd
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-08-17 00:58:18 -0700

    tweak run_single_test.ps1

--- run_single_test.ps1
+++ run_single_test.ps1
@@ -7,10 +7,7 @@ $Env:LOG_ALL_REQUESTS = "true"
 #$Env:ENABLE_FAILING_TESTS = "true"
 #$Env:ENABLE_FLAKY_TESTS = "true"
 
-Set-Location -Path tests
-
-go1.11beta3.exe test -vet=off -v -timeout 30s github.com/ravendb/ravendb-go-client -run ^TestQuery$
+go1.11beta3.exe test -v -vet=off -timeout 30s ./tests -run ^TestFirstClassPatch$
 
 #go1.11beta3.exe test -vet=off -v -timeout 30s github.com/ravendb/ravendb-go-client -run ^TestAttachmentsSession$
 
-Set-Location -Path ..
