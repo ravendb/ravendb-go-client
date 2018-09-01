8a2b4fd380e17a597117ac88b84eff5aac8d3670
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-08-31 23:27:44 -0700

    API cleanup of FacetOptions

--- facet_options.go
+++ facet_options.go
@@ -14,35 +14,3 @@ type FacetOptions struct {
 func FacetOptions_getDefaultOptions() *FacetOptions {
 	return FacetOptions_defaultOptions
 }
-
-func (o *FacetOptions) getTermSortMode() FacetTermSortMode {
-	return o.termSortMode
-}
-
-func (o *FacetOptions) setTermSortMode(termSortMode FacetTermSortMode) {
-	o.termSortMode = termSortMode
-}
-
-func (o *FacetOptions) isIncludeRemainingTerms() bool {
-	return o.includeRemainingTerms
-}
-
-func (o *FacetOptions) setIncludeRemainingTerms(includeRemainingTerms bool) {
-	o.includeRemainingTerms = includeRemainingTerms
-}
-
-func (o *FacetOptions) getStart() int {
-	return o.start
-}
-
-func (o *FacetOptions) setStart(start int) {
-	o.start = start
-}
-
-func (o *FacetOptions) getPageSize() int {
-	return o.pageSize
-}
-
-func (o *FacetOptions) setPageSize(pageSize int) {
-	o.pageSize = pageSize
-}
