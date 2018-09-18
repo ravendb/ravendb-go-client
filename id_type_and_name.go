3793bb6ac3c9df5f70c324b19acf2f3aefd5cdff
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-09-17 22:46:06 -0700

    API cleanup of IdTypeAndName

--- id_type_and_name.go
+++ id_type_and_name.go
@@ -14,36 +14,7 @@ func NewIdTypeAndName(id string, typ CommandType, name string) IdTypeAndName {
 	}
 }
 
-// TODO: remove setter and getter functions after porting most of the code
-func (t *IdTypeAndName) getId() string {
-	return t.id
-}
-
-func (t *IdTypeAndName) setId(id string) {
-	t.id = id
-}
-
-func (t *IdTypeAndName) getType() CommandType {
-	return t.typ
-}
-
-func (t *IdTypeAndName) setType(typ CommandType) {
-	t.typ = typ
-}
-
-func (t *IdTypeAndName) GetName() string {
-	return t.name
-}
-
-func (t *IdTypeAndName) setName(name string) {
-	t.name = name
-}
-
 // TODO: use NewIdTypeAndName instead
 func IdTypeAndName_create(id string, typ CommandType, name string) IdTypeAndName {
-	idTypeAndName := IdTypeAndName{}
-	idTypeAndName.setId(id)
-	idTypeAndName.setType(typ)
-	idTypeAndName.setName(name)
-	return idTypeAndName
+	return NewIdTypeAndName(id, typ, name)
 }
