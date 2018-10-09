907d6d8641d439d61c899652a1ec0e5cf4e708b7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-08 21:01:18 -0700

    add IndexQueryContent

---
+++ index_query_content.go
@@ -0,0 +1,19 @@
+package ravendb
+
+var _ IContent = &IndexQueryContent{}
+
+type IndexQueryContent struct {
+	_conventions *DocumentConventions
+	_query       *IndexQuery
+}
+
+func NewIndexQueryContent(conventions *DocumentConventions, query *IndexQuery) *IndexQueryContent {
+	return &IndexQueryContent{
+		_conventions: conventions,
+		_query:       query,
+	}
+}
+
+func (q *IndexQueryContent) writeContent() map[string]interface{} {
+	return JsonExtensions_writeIndexQuery(q._conventions, q._query)
+}
