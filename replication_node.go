f7529f5e9c879cfeb9d59cd257f421af00b3f9eb
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-08 21:04:22 -0700

    api cleanup of ReplicationNode and ExternalReplication

--- replication_node.go
+++ replication_node.go
@@ -1,31 +1,7 @@
 package ravendb
 
 type ReplicationNode struct {
-	Url      string `json:"Url"`
-	Database string `json:"Database"`
-	Disabled bool   `json:"Disabled"`
-}
-
-func (n *ReplicationNode) getUrl() string {
-	return n.Url
-}
-
-func (n *ReplicationNode) setUrl(url string) {
-	n.Url = url
-}
-
-func (n *ReplicationNode) getDatabase() string {
-	return n.Database
-}
-
-func (n *ReplicationNode) setDatabase(database string) {
-	n.Database = database
-}
-
-func (n *ReplicationNode) isDisabled() bool {
-	return n.Disabled
-}
-
-func (n *ReplicationNode) setDisabled(disabled bool) {
-	n.Disabled = disabled
+	URL        string `json:"Url"`
+	Database   string `json:"Database"`
+	IsDisabled bool   `json:"Disabled"`
 }
