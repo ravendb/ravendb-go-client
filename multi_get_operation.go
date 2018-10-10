0075d4d98e177debf1f835ad298541249231f2b4
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-09 23:08:27 -0700

    add MultiGetOperation

---
+++ multi_get_operation.go
@@ -0,0 +1,18 @@
+package ravendb
+
+type MultiGetOperation struct {
+	_session *InMemoryDocumentSessionOperations
+}
+
+func NewMultiGetOperation(session *InMemoryDocumentSessionOperations) *MultiGetOperation {
+	return &MultiGetOperation{
+		_session: session,
+	}
+}
+
+func (o *MultiGetOperation) createRequest(requests []*GetRequest) *MultiGetCommand {
+	return NewMultiGetCommand(o._session.GetRequestExecutor().GetCache(), requests)
+}
+
+func (o *MultiGetOperation) setResult(result ObjectNode) {
+}
