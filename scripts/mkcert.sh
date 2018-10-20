5504623424042875e48cef61d7ff9a7a508f8fd8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-10-19 20:55:11 -0700

    regenerate certificate and increase cert expiration to 365 days

--- scripts/mkcert.sh
+++ scripts/mkcert.sh
@@ -20,7 +20,7 @@ openssl genrsa -out localhost.key 2048
 
 openssl req -new  -key localhost.key -out localhost.csr -subj "/C=US/ST=Arizona/L=Nevada/O=RavenDB Test/OU=RavenDB test/CN=a.javatest11.development.run/emailAddress=ravendb@example.com"
 
-openssl x509 -req -extensions ext -extfile ../scripts/test_cert.conf -in localhost.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out localhost.crt
+openssl x509 -req -days 365 -extensions ext -extfile ../scripts/test_cert.conf -in localhost.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out localhost.crt
 
 cat localhost.key localhost.crt > cert.pem
 
