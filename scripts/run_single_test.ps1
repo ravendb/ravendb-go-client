87aa04e65754eb0ff0300e1cea348562c6a793b2
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-11-07 18:04:05 -0800

    make run_single_test.ps1 closer to run_single_test.sh

--- scripts/run_single_test.ps1
+++ scripts/run_single_test.ps1
@@ -1,12 +1,17 @@
 
 # go test -vet=off -covermode=atomic -coverprofile=coverage.txt
 
+$Env:VERBOSE_LOG = "true"
 $Env:LOG_HTTP_REQUEST_SUMMARY = "true"
 $Env:LOG_FAILED_HTTP_REQUESTS = "true"
+# logs output of raven server to stdout, helpful for failing tests
+#export LOG_RAVEN_SERVER=true
 $Env:LOG_ALL_REQUESTS = "true"
 $Env:ENABLE_FAILING_TESTS = "false"
 $Env:ENABLE_FLAKY_TESTS = "false"
+
 $Env:RAVENDB_JAVA_TEST_SERVER_PATH = "$PSScriptRoot\..\RavenDB\Server\Raven.Server.exe"
+
 #$Env:RAVEN_GO_NO_DB_TESTS = "no"
 
 go.exe clean -testcache
