160d59dc77ef6e96bc30ce3c20f6e02daa222dbd
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-10 15:46:18 -0800

    implement cofi_can_avoid_using_server_for_load_with_include_if_everything_is_in_session_cacheLazy

--- lazy_multi_loader_with_include.go
+++ lazy_multi_loader_with_include.go
@@ -1,31 +1,93 @@
 package ravendb
 
-import "reflect"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
 
+// LazyMultiLoaderWithInclude is for lazily loading one or more objects with includes
 type LazyMultiLoaderWithInclude struct {
 	_session  *DocumentSession
 	_includes []string
 }
 
+// NewLazyMultiLoaderWithInclude creates a lazy multi loader with includes
 func NewLazyMultiLoaderWithInclude(session *DocumentSession) *LazyMultiLoaderWithInclude {
 	return &LazyMultiLoaderWithInclude{
 		_session: session,
 	}
 }
 
+// Include adds ids of objects to add in a request
 func (l *LazyMultiLoaderWithInclude) Include(path string) *ILazyLoaderWithInclude {
 	l._includes = append(l._includes, path)
 	return l
 }
 
-// Lazy<Map<String, T>>
+// LoadMulti lazily loads multiple values of a given type with given ids
 func (l *LazyMultiLoaderWithInclude) LoadMulti(clazz reflect.Type, ids []string) *Lazy {
 	return l._session.lazyLoadInternal(clazz, ids, l._includes, nil)
 }
 
-/*
-   public <TResult> Lazy<TResult> load(Class<TResult> clazz, String id) {
-       Lazy<Map<String, TResult>> results = _session.lazyLoadInternal(clazz, new String[]{id}, _includes.toArray(new String[0]), null);
-       return new Lazy(() -> results.getValue().values().iterator().next());
-   }
-*/
+// m is a single-element map[string]*struct
+// returns single map value
+func getOneMapValue(results interface{}) (interface{}, error) {
+	m := reflect.ValueOf(results)
+	if m.Type().Kind() != reflect.Map {
+		return nil, fmt.Errorf("results should be a map[string]*struct, is %s. tp: %s", m.Type().String(), m.Type().String())
+	}
+	mapKeyType := m.Type().Key()
+	if mapKeyType != stringType {
+		return nil, fmt.Errorf("results should be a map[string]*struct, is %s. tp: %s", m.Type().String(), m.Type().String())
+	}
+	mapElemPtrType := m.Type().Elem()
+	if mapElemPtrType.Kind() != reflect.Ptr {
+		return nil, fmt.Errorf("results should be a map[string]*struct, is %s. tp: %s", m.Type().String(), m.Type().String())
+	}
+
+	mapElemType := mapElemPtrType.Elem()
+	if mapElemType.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("results should be a map[string]*struct, is %s. tp: %s", m.Type().String(), m.Type().String())
+	}
+	keys := m.MapKeys()
+	if len(keys) == 0 {
+		return nil, nil
+	}
+	if len(keys) != 1 {
+		return nil, fmt.Errorf("expected results to have only one element, has %d", len(keys))
+	}
+	v := m.MapIndex(keys[0])
+	return v.Interface(), nil
+}
+
+// Load lazily loads a value of a given type with a given id
+func (l *LazyMultiLoaderWithInclude) Load(clazz reflect.Type, id string) *Lazy {
+	ids := []string{id}
+	results := l._session.lazyLoadInternal(clazz, ids, l._includes, nil)
+	valueFactory := func() (interface{}, error) {
+		// resultMap is map[string]clazz
+		resultMap, err := results.GetValue()
+		if err != nil {
+			return nil, err
+		}
+		if m, ok := resultMap.(map[string]interface{}); ok {
+			if len(m) == 0 {
+				return nil, nil
+			}
+			panicIf(len(m) != 1, "expected resultMap to only have one element, has %d", len(m))
+			for _, v := range m {
+				return v, nil
+			}
+			return nil, errors.New("impossible!")
+		}
+
+		//fmt.Printf("resultMap: %v, type=%T\n", resultMap, resultMap)
+		// TODO: not sure if this is needed
+		v, err := getOneMapValue(resultMap)
+		fmt.Printf("LazyMultiLoaderWithInclude.Load(): v=%v, type=%T\n", v, v)
+		must(err)
+		return v, nil
+	}
+	return NewLazy(valueFactory)
+}
