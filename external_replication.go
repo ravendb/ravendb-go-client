ad37e3a0fae645dba70745a84c6c6ea5d2b8862d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-10 16:19:36 -0800

    fix typo

--- external_replication.go
+++ external_replication.go
@@ -1,13 +1,15 @@
 package ravendb
 
+// ExternalReplication describes external replication
 type ExternalReplication struct {
 	ReplicationNode
 	TaskID               int    `json:"TaskId"`
 	Name                 string `json:"Name"`
-	ConnectionstringName string `json:"ConnectionstringName"`
+	ConnectionstringName string `json:"ConnectionStringName"`
 	MentorName           string `json:"MentorName"`
 }
 
+// NewExternalReplication creates ExternalReplication
 func NewExternalReplication(database string, connectionstringName string) *ExternalReplication {
 	return &ExternalReplication{
 		ReplicationNode: ReplicationNode{
--- replication_node.go
+++ replication_node.go
@@ -1,5 +1,6 @@
 package ravendb
 
+// ReplicationNode describes replication node
 type ReplicationNode struct {
 	URL        string `json:"Url"`
 	Database   string `json:"Database"`
