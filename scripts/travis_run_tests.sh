bd327542f548eb344c2910654c06d72ff9a17ab1
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-17 23:13:58 -0800

    fix travis_run_tests.sh for appveyor

--- scripts/travis_run_tests.sh
+++ scripts/travis_run_tests.sh
@@ -15,9 +15,8 @@ export RAVENDB_JAVA_TEST_CERTIFICATE_PATH="${wd}/certs/server.pfx"
 export RAVENDB_JAVA_TEST_CLIENT_CERTIFICATE_PATH="${wd}/certs/cert.pem"
 export RAVENDB_JAVA_TEST_HTTPS_SERVER_URL="https://a.javatest11.development.run:8085"
 
-echo "BUILD_DIR: $TRAVIS_BUILD_DIR"
-echo "pwd:              `pwd`"
-echo "GOPATH:           $GOPATH"
+echo "pwd:              ${wd}"
+echo "GOPATH:           ${GOPATH}"
 
 # go test -v -race  -vet=off -coverpkg=all -covermode=atomic -coverprofile=coverage.txt ./tests
 go test -v -parallel 1 -timeout 20m -race -coverpkg=github.com/ravendb/ravendb-go-client -covermode=atomic -coverprofile=coverage.txt . ./tests
