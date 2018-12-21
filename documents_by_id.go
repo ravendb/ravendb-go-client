54ec5244c957e5b647bb94b8401566aeadcf5132
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-20 23:17:58 -0800

    make documentsByID.add match Java

--- documents_by_id.go
+++ documents_by_id.go
@@ -16,16 +16,9 @@ func (d *documentsByID) getValue(id string) *documentInfo {
 }
 
 func (d *documentsByID) add(info *documentInfo) {
-	// TODO: this happens in Java but in Go it breaks change tracking
-	// e.g. crudTest_crudOperationsWithNull
-	// Not sure if should restore this and the bug is caused by some
-	// other code path
-	// see investigation notes in https://www.notion.so/Investigation-crudTest_crudOperationsWithNull-failure-a37969d8118e4eb39635e458a369ab09
-	/*
-		if _, ok := d.inner[info.id]; ok {
-			return
-		}
-	*/
+	if _, ok := d.inner[info.id]; ok {
+		return
+	}
 
 	d.inner[info.id] = info
 }
