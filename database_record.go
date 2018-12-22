9c3ead2bb369a8e48e511fb29dc73784ec5047e0
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-21 17:43:48 -0800

    cleanup DatabaseRecord and DatabaseRecordWithEtag

--- database_record.go
+++ database_record.go
@@ -1,13 +1,15 @@
 package ravendb
 
+// DatabaseRecord represents database record
 type DatabaseRecord struct {
 	DatabaseName         string            `json:"DatabaseName"`
 	Disabled             bool              `json:"Disabled"`
-	DataDirectory        *string           `json:"DataDirectory"`
+	DataDirectory        string            `json:"DataDirectory,omitempty"`
 	Settings             map[string]string `json:"Settings"`
 	ConflictSolverConfig *conflictSolver   `json:"ConflictSolverConfig"`
 }
 
+// NewDatabaseRecord returns new database record
 func NewDatabaseRecord() *DatabaseRecord {
 	return &DatabaseRecord{
 		Settings: map[string]string{},
--- database_record_with_etag.go
+++ database_record_with_etag.go
@@ -1,16 +1,7 @@
 package ravendb
 
+// DatabaseRecordWithEtag represents database record with etag
 type DatabaseRecordWithEtag struct {
-	// TODO: should I just replicate the fields? Not sure how json deals with this
 	DatabaseRecord
-
 	Etag int `json:"Etag"`
-}
-
-func (r *DatabaseRecordWithEtag) getEtag() int {
-	return r.Etag
-}
-
-func (r *DatabaseRecordWithEtag) setEtag(etag int) {
-	r.Etag = etag
 }
