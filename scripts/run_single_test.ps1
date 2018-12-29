9ce91dae27f5e2f9107cee4d23444cdfc8e93e5e
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-28 20:29:16 -0800

    fix run_single_test.ps1 for windows and remove scripts/run_single_test.sh

--- scripts/run_single_test.ps1
+++ scripts/run_single_test.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-# go test -covermode=atomic -coverprofile=coverage.txt
+# on mac install powershell: https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-macos?view=powershell-6
 
 $Env:VERBOSE_LOG = "true"
 $Env:LOG_HTTP_REQUEST_SUMMARY = "true"
@@ -11,20 +11,19 @@ $Env:LOG_ALL_REQUESTS = "true"
 $Env:ENABLE_FAILING_TESTS = "false"
 $Env:ENABLE_FLAKY_TESTS = "false"
 
-$wd = Join-Path $PSScriptRoot ".."
-$ravdir = Join-Path $wd "RavenDB" "Server"
-
+# $IsMacOS is only defined in powershell 6, but it happens to work
+# in windows with powershell 5 because it's not defined at all, so false
 if ($IsMacOS) {
+    $ravdir = "RavenDB/Server"
     $Env:RAVENDB_JAVA_TEST_SERVER_PATH = "$ravdir/Raven.Server"
     $Env:RAVENDB_JAVA_TEST_CERTIFICATE_PATH="${wd}/certs/server.pfx"
     $Env:RAVENDB_JAVA_TEST_CLIENT_CERTIFICATE_PATH="${wd}/certs/cert.pem"
     $Env:RAVENDB_JAVA_TEST_HTTPS_SERVER_URL="https://a.javatest11.development.run:8085"
-
 } else {
+    $ravdir = Join-Path -Path "$PSScriptRoot" -ChildPath ".." -Resolve
+    $ravdir = "$ravdir\RavenDB\Server"
     $Env:RAVENDB_JAVA_TEST_SERVER_PATH = "$ravdir\Raven.Server.exe"
 }
-
-#$Env:RAVEN_GO_NO_DB_TESTS = "no"
 
 go clean -testcache
 
