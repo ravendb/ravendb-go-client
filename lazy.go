1b1fb7a8358e21d0c04104ab81012277e8103860
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-28 17:53:11 -0800

    start on strongly-typed interface for lazy values

--- lazy.go
+++ lazy.go
@@ -4,10 +4,12 @@ import "sync"
 
 // Lazy represents a lazy operation
 type Lazy struct {
-	valueFactory func() (interface{}, error)
-	valueCreated bool
-	value        interface{}
-	mu           sync.Mutex
+	valueFactory  func() (interface{}, error)
+	valueFactory2 func(interface{}) error
+	valueCreated  bool
+	value         interface{}
+	err           error
+	mu            sync.Mutex
 }
 
 // NewLazy returns new Lazy value
@@ -17,10 +19,18 @@ func NewLazy(valueFactory func() (interface{}, error)) *Lazy {
 	}
 }
 
+// NewLazy2 returns new Lazy value
+func NewLazy2(valueFactory func(interface{}) error) *Lazy {
+	return &Lazy{
+		valueFactory2: valueFactory,
+	}
+}
+
 // IsValueCreated returns true if lazy value has been created
 func (l *Lazy) IsValueCreated() bool {
 	l.mu.Lock()
 	defer l.mu.Unlock()
+
 	return l.valueCreated
 }
 
@@ -28,10 +38,40 @@ func (l *Lazy) IsValueCreated() bool {
 func (l *Lazy) GetValue() (interface{}, error) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
-	var err error
+
+	if l.err != nil {
+		panicIf(!l.valueCreated, "l.valuerCreated must have been set")
+		return nil, l.err
+	}
+
 	if !l.valueCreated {
-		l.value, err = l.valueFactory()
+		l.value, l.err = l.valueFactory()
 		l.valueCreated = true
 	}
-	return l.value, err
+	return l.value, l.err
+}
+
+// GetValue2 returns a value of lazy operation
+func (l *Lazy) GetValue2(v interface{}) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.err != nil {
+		panicIf(!l.valueCreated, "l.valuerCreated must have been set")
+		return l.err
+	}
+
+	if l.valueCreated {
+		panic("not yet supporting calling multiple times")
+
+	}
+	if !l.valueCreated {
+		l.err = l.valueFactory2(v)
+		l.value = v // TODO: this should convert **struct => *struct
+		l.valueCreated = true
+	} else {
+		setInterfaceToValue(v, l.value)
+	}
+
+	return l.err
 }
