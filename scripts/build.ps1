e5d516c64838313182f72d4a4796f740fd7085ed
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-12-28 17:32:21 -0800

    remove scripts/build.sh and make scripts/build.ps1 work on mac

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,5 @@
+#!/usr/bin/env pwsh
+
 # sanity check: make sure that code and tests do compile
 # compiling tests will also compile the code
-
-go test -v -c .\tests
+go test -v -c ./tests
--- scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# sanity check: make sure that code and tests do compile
-# compiling tests will also compile the code
-
-go test -v -c .
-go test -v -c ./tests
