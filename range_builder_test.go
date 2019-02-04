11763a7be8bb2b6fdeaeac6bd2950a59c243c152
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-02-03 19:17:36 -0800

    more test coverage for RangeBuilder

--- range_builder_test.go
+++ range_builder_test.go
@@ -11,6 +11,7 @@ func TestRangeBuilder(t *testing.T) {
 		b := NewRangeBuilder("foo")
 		b = b.IsLessThan(3)
 		b = b.IsLessThan(4)
+		b = b.IsLessThan(5)
 		assert.Error(t, b.Err())
 		assert.True(t, strings.Contains(b.Err().Error(), "Less bound was already set"))
 	}
@@ -19,6 +20,7 @@ func TestRangeBuilder(t *testing.T) {
 		b := NewRangeBuilder("foo")
 		b = b.IsLessThan(3)
 		b = b.IsLessThanOrEqualTo(18)
+		b = b.IsLessThanOrEqualTo(22)
 		assert.Error(t, b.Err())
 		assert.True(t, strings.Contains(b.Err().Error(), "Less bound was already set"))
 	}
@@ -27,6 +29,7 @@ func TestRangeBuilder(t *testing.T) {
 		b := NewRangeBuilder("foo")
 		b = b.IsGreaterThan(3)
 		b = b.IsGreaterThan(18)
+		b = b.IsGreaterThan(3)
 		assert.Error(t, b.Err())
 		assert.True(t, strings.Contains(b.Err().Error(), "Greater bound was already set"))
 	}
@@ -35,8 +38,16 @@ func TestRangeBuilder(t *testing.T) {
 		b := NewRangeBuilder("foo")
 		b = b.IsGreaterThanOrEqualTo(3)
 		b = b.IsGreaterThan(18)
+		b = b.IsGreaterThanOrEqualTo(12)
 		assert.Error(t, b.Err())
 		assert.True(t, strings.Contains(b.Err().Error(), "Greater bound was already set"))
+
+		addQueryParameter := func(interface{}) string {
+			return ""
+		}
+		_, err := b.GetStringRepresentation(addQueryParameter)
+		assert.Error(t, err)
+		assert.True(t, strings.Contains(err.Error(), "Greater bound was already set"))
 	}
 
 	{
