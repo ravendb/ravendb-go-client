62a63d290587de8a9705e1d9c993668ec9df1647
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-02-19 19:12:42 -0800

    add code to randomly kill a node in a cluster

--- scripts/run_java_tests.ps1
+++ scripts/run_java_tests.ps1
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-$Env:LOG_HTTP_REQUEST_SUMMARY = "true"
-$Env:LOG_FAILED_HTTP_REQUESTS = "true"
-$Env:LOG_ALL_REQUESTS = "true"
-#$Env:ENABLE_FAILING_TESTS = "true"
-#$Env:ENABLE_FLAKY_TESTS = "true"
 $Env:RAVENDB_JAVA_TEST_SERVER_PATH = "$PSScriptRoot\..\RavenDB\Server\Raven.Server.exe"
 $Env:HTTP_PROXY = "http://localhost:8888"
 
