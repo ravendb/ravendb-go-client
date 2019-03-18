767d71d254bbb65850f0a17b148bdd2ee3064e8d
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2019-03-18 14:03:10 -0700

    appveyor: set logging env variables in appveyor.yml so that they are the same on linux/windows and we log requests on windows

--- scripts/appveyor_run_tests.sh
+++ scripts/appveyor_run_tests.sh
@@ -2,10 +2,10 @@
 set -u -o pipefail -o xtrace
 
 # comment/uncomment to turn additional logging on/off
-export VERBOSE_LOG=true
-export LOG_FAILED_HTTP_REQUESTS=true
-export LOG_ALL_REQUESTS=true
-export LOG_FAILED_HTTP_REQUESTS_DELAYED=true
+#export VERBOSE_LOG=true
+#export LOG_FAILED_HTTP_REQUESTS=true
+#export LOG_ALL_REQUESTS=true
+#export LOG_FAILED_HTTP_REQUESTS_DELAYED=true
 #export ENABLE_FAILING_TESTS=true
 #export ENABLE_FLAKY_TESTS=true
 # must use full absolute path because working directory is direrectory of
@@ -15,7 +15,7 @@ export RAVENDB_TEST_CERTIFICATE_PATH="${wd}/certs/server.pfx"
 export RAVENDB_TEST_CA_PATH="${wd}/certs/ca.crt"
 export RAVENDB_TEST_CLIENT_CERTIFICATE_PATH="${wd}/certs/cert.pem"
 export RAVENDB_TEST_HTTPS_SERVER_URL="https://a.javatest11.development.run:8085"
-export RAVENDB_SERVER_VERSION="4.1.3" # see appveyor.yml
+#export RAVENDB_SERVER_VERSION="4.1.3" # see appveyor.yml
 
 #export NODES_IN_CLUSTER="3"
 #export KILL_SERVER_CHANCE="10"
