a0710e2acd5dbde438ebd7988b51c1eddb0bf5b4
omerlewitz <omer@ravendb.net>
2021-09-05 08:20:20 +0300

    RDBC-473 - implementation of cluster operation

---
+++ serverwide/operations/operation_demote_cluster_node.go
@@ -0,0 +1,36 @@
+package operations
+
+import (
+	"encoding/json"
+	"github.com/ravendb/ravendb-go-client"
+	"net/http"
+)
+
+type OperationDemoteClusterNode struct {
+	Node string `json:"Node"`
+}
+
+func (operation *OperationDemoteClusterNode) GetCommand(conventions *ravendb.DocumentConventions) (ravendb.RavenCommand, error) {
+	return &demoteNodeCommand{
+		RaftCommandBase: ravendb.RaftCommandBase{
+			RavenCommandBase: ravendb.RavenCommandBase{
+				ResponseType: ravendb.RavenCommandResponseTypeObject,
+			},
+		},
+		parent: operation,
+	}, nil
+}
+
+type demoteNodeCommand struct {
+	ravendb.RaftCommandBase
+	parent *OperationDemoteClusterNode
+}
+
+func (c *demoteNodeCommand) createRequest(node *ravendb.ServerNode) (*http.Request, error) {
+	url := node.URL + "/admin/cluster/demote?nodeTag=" + c.parent.Node
+	return http.NewRequest(http.MethodPost, url, nil)
+}
+
+func (c *demoteNodeCommand) setResponse(response []byte, fromCache bool) error {
+	return json.Unmarshal(response, c.parent)
+}
