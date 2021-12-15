8cbbb46befa907283d01bc11e3caed4966544bfa
Omer Michlewitz <omer@ravendb.net>
2021-12-15 14:39:35 +0200

    idiomatic go enums to certificates package and formatting.

--- tests/operation_put_certificate_test.go
+++ tests/operation_put_certificate_test.go
@@ -10,6 +10,7 @@ import (
 
 func putCertificateTest(t *testing.T, driver *RavenTestDriver) {
 	var err error
+
 	store := driver.getSecuredDocumentStoreMust(t)
 	assert.NotNil(t, store)
 	defer store.Close()
@@ -19,16 +20,19 @@ func putCertificateTest(t *testing.T, driver *RavenTestDriver) {
 		fmt.Printf("Didn't find cert.crt file at '%s'. Set RAVENDB_TEST_CA_PATH env variable\n", path)
 		os.Exit(1)
 	}
+
 	certificate := loadTestCaCertificate(path)
 	assert.NotNil(t, certificate)
 	fmt.Printf("Loaded client certificate from '%s'\n", path)
 
+	certName := "Admin Certificate"
 	operation := certificates.OperationPutCertificate{
-		CertName:          "Admin Certificate",
+		CertName:          certName,
 		CertBytes:         certificate.Raw,
-		SecurityClearance: certificates.SecurityClearance.Operator,
+		SecurityClearance: certificates.ClusterAdmin.String(),
 		Permissions:       nil,
 	}
+
 	err = store.Maintenance().Server().Send(&operation)
 	assert.NoError(t, err)
 
